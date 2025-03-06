3d1251e4456e15fbb59df7cfd235db32d1f53f45
PHOscar <phaith-ann.hinds@cynatglobal.com>
2025-03-06 13:01:21 +0000

    git add comments.js

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create web server
+const express = require('express');
+const app = express();
+
+// Set up the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+}); 
